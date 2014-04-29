ae1b78cab8964367c2db8222ae99eed61c5e0ee6
Alex Whitney <alex.whitney@cantab.net>
2014-04-29 18:49:31 -0400

    rendering working

--- univ.rs
+++ univ.rs
@@ -7,9 +7,8 @@ static WIDTH: int = 1024;
 static HEIGHT: int = 768;
 static COLDEPTH: int = 32;
 static FPS: int = 60;
-static DT: f64 = 1.0;
-static EPS: f64 = 0.001;
-static BPP: int = 4; 
+static DT: f64 = 0.2;
+static EPS: f64 = 0.1;
 
 struct Particle {
     pos : Vector,
@@ -27,14 +26,14 @@ fn force(p1: &Particle, p2: &Particle) -> Vector {
     let disp = diff(p1.pos, p2.pos);
     let dist = modls(disp);
     let f = p1.mass * p2.mass / (dist + EPS); // force magnitude
-    Vector { x: f*dist/disp.x, y: f*dist/disp.y }
+    Vector { x: -f*disp.x/dist, y: -f*disp.y/dist }
 }
 
-fn step(m: &mut Particle, M: &mut Particle) {
+fn step(m: &mut Particle, M: &Particle) {
     // assume M doesn't change
     // update positions
-    m.pos.x = m.pos.x + m.vel.x;
-    m.pos.y = m.pos.y + m.vel.y;
+    m.pos.x = m.pos.x + m.vel.x*DT;
+    m.pos.y = m.pos.y + m.vel.y*DT;
     // update velocities
     let f = force(m, M);
     m.vel.x += f.x/m.mass*DT;
@@ -55,12 +54,11 @@ fn diff(v1 : Vector, v2: Vector) -> Vector {
 
 fn circle_points(x: f64, y: f64, r:f64) -> Vec<Point> {
     let mut points: Vec<Point> = Vec::new();
-    let mut dy = 1;
     let rr = r as i32;
     let xr = x as i32;
     let yr = y as i32;
-    while dy <= rr {
-        let xlim = ((rr - dy*dy) as f64).sqrt() as i32;
+    for dy in range(0,rr) {
+        let xlim = ((rr*rr - dy*dy) as f64).sqrt() as i32;
         for dx in range (0, xlim) {
             points.push(Point {x:xr + dx, y: yr + dy});
             points.push(Point {x:xr - dx, y: yr + dy});
@@ -71,7 +69,7 @@ fn circle_points(x: f64, y: f64, r:f64) -> Vec<Point> {
     points
 }
 
-fn main() {
+fn animate() {
     sdl2::init(sdl2::InitVideo);
 
     let window = match sdl2::video::Window::new("Univ", sdl2::video::PosCentered, sdl2::video::PosCentered, WIDTH, HEIGHT, sdl2::video::OpenGL) {
@@ -84,15 +82,47 @@ fn main() {
         Err(err) => fail!(format!("failed to create renderer: {}", err))
     };
 
-    renderer.clear();
-    renderer.draw_points(circle_points(0.,0.,10.).as_slice());
-    renderer.present();
-    sleep(1000);
+    let mut p1 = Particle { pos:Vector {x: 100., y: 0.}, vel:Vector {x:0.,y: 0.5}, mass:1. };
+    let mut p2 = Particle { pos:Vector {x:-100., y: 0.}, vel:Vector {x:0.,y:-0.5}, mass:1. };
 
+    let midx = (WIDTH/2) as f64;
+    let midy = (HEIGHT/2) as f64;
+
+    'main: loop {
+        renderer.clear();
+        renderer.draw_points(circle_points(p1.pos.x+midx, p1.pos.y+midy, p1.mass*10.).as_slice());
+        renderer.draw_points(circle_points(p2.pos.x+midx, p2.pos.y+midy, p2.mass*10.).as_slice());
+        renderer.present();
+        step(&mut p1, &p2);
+        step(&mut p2, &p1);
+        match sdl2::event::poll_event() {
+            sdl2::event::QuitEvent(_) => break 'main,
+            sdl2::event::KeyDownEvent(_, _, key, _, _) => {
+                if key == sdl2::keycode::EscapeKey {
+                    break 'main
+                }
+            }
+            _ => {}
+        }
+    }
     sdl2::quit();
-
-    //let mut m1 = Particle { pos:Vector {x: 1, y: 1}, vel:Vector {x:0,y:0}, mass:1 };
-    //let mut m2 = Particle { pos:Vector {x:-1, y:-1}, vel:Vector {x:0,y:0}, mass:1 };
-
 }
 
+fn main() {
+    animate()
+}
+/*
+    let mut p1 = Particle { pos:Vector {x: 100., y: 0.}, vel:Vector {x:0.,y:0.}, mass:1. };
+    let mut p2 = Particle { pos:Vector {x:-100., y: 0.}, vel:Vector {x:0.,y:0.}, mass:1. };
+
+    let midx = (WIDTH/2) as f64;
+    let midy = (HEIGHT/2) as f64;
+
+    for _ in range(0,10) {
+        step(&mut p1, &p2);
+        step(&mut p2, &p1);
+        println!("P1:{:?}    P2: {:?}", p1, p2);
+    }
+}
+
+*/
