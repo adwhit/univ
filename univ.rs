c5762bf429b4ca525de879871e18c3cdd7ef0a5a
Alex Whitney <aw1209@ic.ac.uk>
2014-05-30 16:51:08 +0100

    working towards better accuracy

--- univ.rs
+++ univ.rs
@@ -11,26 +11,36 @@ static WIDTH: uint = 1024;
 static HEIGHT: uint = 768;
 static FPS: int = 60;
 static DT: f64 = 0.1;
-static EPS: f64 = 0.1;
+static EPS: f64 = 0.;
 static NBYTES: uint = 4;
 
+#[deriving(Eq)]
 struct Particle {
     pos : Vector,
     vel : Vector,
     mass: f64
 }
 
+#[deriving(Eq)]
 struct Vector {
     x : f64,
     y : f64
 }
 
+impl Vector {
+    fn add(&mut self, other: &Vector) {
+        self.x += other.x;
+        self.y += other.y;
+    }
+}
 
+
+//force is calculated as pointing from particle 1 towards particle 2
 fn force(p1: &Particle, p2: &Particle) -> Vector {
     let disp = diff(p1.pos, p2.pos);
-    let dist = modls(disp);
-    let f = p1.mass * p2.mass / (dist + EPS); // force magnitude
-    Vector { x: -f*disp.x/dist, y: -f*disp.y/dist }
+    let dist = modls(disp) + EPS;
+    let f = p1.mass * p2.mass / dist; // force magnitude
+    Vector { x: f*disp.x/dist, y: f*disp.y/dist }
 }
 
 fn steppos(m: &mut Particle) {
@@ -46,8 +56,9 @@ fn modls(v : Vector) -> f64 {
     (v.x * v.x + v.y* v.y).sqrt()
 }
 
+//vector pointing from v1 towards v2
 fn diff(v1 : Vector, v2: Vector) -> Vector {
-    Vector { x: v1.x - v2.x, y: v1.y -v2.y }
+    Vector { x: v2.x - v1.x, y: v2.y -v1.y }
 }
 
 fn circle_points(x: f64, y: f64, r:f64) -> Vec<Point> {
@@ -70,15 +81,19 @@ fn circle_points(x: f64, y: f64, r:f64) -> Vec<Point> {
 fn make_galaxy() -> Vec<Particle> {
     let mut particles: Vec<Particle> = Vec::new();
     let pi =  f64::consts::PI;
+    // n planets at radius r
     let ns = [20,  50,   100,  150,  200];
     let rs = [50., 100., 200., 250., 300.];
+    //parameter to weight velocities
     let velweight = 400.0;
 
     for (&n, &r) in ns.iter().zip(rs.iter()) {
         for i in std::iter::range_inclusive(1, n) {
             let theta = (i as f64)/(n as f64)*2.0*pi;
+            //XXX want to change to make universe stable
             let vel = velweight*(1.0/r).sqrt();
             particles.push(Particle {pos:Vector {x: r*theta.cos(),   y: r*theta.sin()  }, 
+                                     //vel:Vector {x: 0., y: 0.},
                                      vel:Vector {x: vel*theta.sin(), y: -vel*theta.cos()},
                                      mass:1. });
         }
@@ -87,6 +102,41 @@ fn make_galaxy() -> Vec<Particle> {
     particles.push(Particle {pos:Vector {x: 0., y: 0.  }, 
                              vel:Vector {x: 0., y: 0.}, mass:1000. });
     particles
+}
+
+fn init_velocity(particles: &mut Vec<Particle>) {
+    for p in particles.iter() {
+        let r = modls(p.pos);
+        let mut tot_force = Vector {x : 0., y: 0.};
+        for q in particles.iter() {
+            if q != p {
+                tot_force.add(&force(p, q))
+            }
+        }
+        //p.vel = tot_force;
+    }
+}
+
+//Calculates particle with equivalent centre of mass and total mass
+fn centre_of_mass(particles: Vec<Particle>) -> Particle {
+    //position
+    let mut rx = 0.;
+    let mut ry = 0.;
+    //momentum
+    let mut px = 0.;
+    let mut py = 0.;
+    //mass
+    let mut m = 0.;
+    for p in particles.iter() {
+        rx += p.pos.x*p.mass;
+        ry += p.pos.y*p.mass;
+        px += p.vel.x*p.mass;
+        py += p.vel.y*p.mass;
+        m += p.mass;
+    }
+    return Particle {pos: Vector {x: rx/m, y: ry/m},
+                     vel: Vector {x: px/m, y: py/m},
+                     mass: m}
 }
 
 fn stepvel(force: Vector, p: &mut Particle, sense:bool) {
@@ -159,12 +209,13 @@ fn animate() {
 
     renderer.clear();
     loop {
+        renderer.clear();
         stepsim(&mut particles, lenp);
         let points = pcls2points(&particles);
-        mask.update(None, pcls2pixel(&particles), (WIDTH*NBYTES) as int);
-        renderer.copy(&mask,None,None);
-        //renderer.draw_points(points);
-        renderer.draw_line(Point::new(1,2),Point::new(30, 50));
+        //mask.update(None, pcls2pixel(&particles), (WIDTH*NBYTES) as int);
+        //renderer.copy(&mask,None,None);
+        renderer.draw_points(points.as_slice());
+        //renderer.draw_line(Point::new(1,2),Point::new(30, 50));
         renderer.present();
         match sdl2::event::poll_event() {
             sdl2::event::QuitEvent(_) => break,
