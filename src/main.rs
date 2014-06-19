b940d7de971c9775af8d6cf163b49dd9eb120164
Alex Whitney <aw1209@ic.ac.uk>
2014-06-19 17:59:16 +0100

    update sdl and rust version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 extern crate sdl2;
-extern crate time;
 
 use sdl2::rect::Point;
 use sdl2::pixels::{RGB, RGBA};
@@ -31,8 +30,8 @@ fn circle_points(x: f64, y: f64, r:f64) -> Vec<Point> {
     points
 }
 
-fn pcls2pixel(particles: &Vec<Particle>) -> ~[u8] {
-    let mut arr : ~[u8] = ~[0,..NBYTES*WIDTH*HEIGHT];
+fn pcls2pixel(particles: &Vec<Particle>) -> Vec<u8> {
+    let mut arr : Vec<u8> = Vec::from_fn(NBYTES*WIDTH*HEIGHT, |x| 0);
     let midx = (WIDTH/2) as f64;
     let midy = (HEIGHT/2) as f64;
     for p in particles.iter() {
@@ -40,9 +39,9 @@ fn pcls2pixel(particles: &Vec<Particle>) -> ~[u8] {
         let yind = (p.pos.y + midy) as uint;
         if xind < WIDTH && yind < HEIGHT {
             let ix = NBYTES * ((yind * WIDTH) + xind);
-            arr[ix] = 0xff;
-            arr[ix+1] = 0xff;
-            arr[ix+2] = 0xff;
+            *arr.get_mut(ix) = 0xff;
+            *arr.get_mut(ix+1) = 0xff;
+            *arr.get_mut(ix+2) = 0xff;
         }
     }
     arr
@@ -69,7 +68,7 @@ fn animate() {
     particles.push_all(galaxy1.as_slice());
     let lenp = particles.len();
     let mut framect = 0;
-    let pixels : ~[u8] = ~[0,..NBYTES*WIDTH*HEIGHT];
+    let pixels : Vec<u8> = Vec::with_capacity(NBYTES*WIDTH*HEIGHT);
 
     renderer.clear();
     loop {
